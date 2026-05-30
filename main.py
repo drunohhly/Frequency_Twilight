cd80be1a27feaa0db5a5898e94f33ffc34ebd4cb
sonikulagija <gosofakula007@gmail.com>
2026-05-31 00:44:07 +0300

    Tokenization
    Токенизация текста

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 text = open("text.txt", "r", encoding="utf-8")
 text = text.read()
 
+#очищение текста
 text = text.lower()
 text = re.sub(r'[^а-яё\s]', '', text)
 text = re.sub(r'\s+', ' ', text)
+
+#токенизация
+nltk.download('punkt_tab')
+tokens = word_tokenize(text)
+print(tokens)
